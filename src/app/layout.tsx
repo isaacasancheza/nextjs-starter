56b7fd737f07d1277364c6604f3f985ef8a6b35c
Isaac Sánchez <1031819+isaacasancheza@users.noreply.github.com>
2025-01-20 17:36:59 -0600

    :recycle: rename function to avoid name collision

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import '@/styles/global.css'
 import type { Metadata } from 'next'
+import { ReactNode } from 'react'
 
 export const metadata: Metadata = {
   title: 'NextJS Starter',
   description: 'A simple starter to get up and developing quickly with NextJS',
 }
 
-interface Layout {
-  children: React.ReactNode
+interface RootLayout {
+  children: ReactNode
 }
 
-export default function Layout({ children }: Layout) {
+export default function RootLayout({ children }: RootLayout) {
   return (
     <html lang="es-MX" className="h-full">
       <body className="h-full">
