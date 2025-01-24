4eb34a4f313bfd80ba54cc832c62dad318bed3ae
Isaac Sánchez <1031819+isaacasancheza@users.noreply.github.com>
2025-01-24 17:16:40 -0600

    :bug: fix tailwind config and remove container plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,10 @@
 import aspectRatio from '@tailwindcss/aspect-ratio'
-import containerQueries from '@tailwindcss/container-queries'
 import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  mode: 'jit',
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {},
-  },
-  corePlugins: {
-    aspectRatio: false,
-  },
-  plugins: [forms, typography, aspectRatio, containerQueries],
+  plugins: [forms, typography, aspectRatio],
 }
 
 export default config
