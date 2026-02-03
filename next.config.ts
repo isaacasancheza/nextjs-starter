bdb342fc36fe2a70f28f2f72221c244e67f5d30a
Isaac Sánchez <isaac@elrincondelsanchez.com>
2026-02-02 21:13:43 -0600

    :arrow_up: ugprade next

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   typedRoutes: true,
   cacheComponents: true,
+  serverExternalPackages: ['import-in-the-middle'],
 }
 
 export default nextConfig
